4fcab44e4e9731cd2698b154bef5704856d38485
Yicheng Qin <qycqycqycqycqyc@gmail.com>
2017-01-13 00:30:41 -0800

    add cache for compile result

--- compile.go
+++ compile.go
@@ -1,10 +1,14 @@
 package replay
 
 import (
+	"bytes"
+	"encoding/json"
 	"io"
+	"io/ioutil"
 	"net/http"
 
 	"appengine"
+	"appengine/memcache"
 	"appengine/urlfetch"
 )
 
@@ -12,15 +16,63 @@ const compileURL = "https://sandbox-dot-replay-154206.appspot-preview.com/compil
 
 func init() { http.HandleFunc("/compile", compile) }
 
+type result struct {
+	Status  int
+	Message []byte
+}
+
 func compile(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
 	client := urlfetch.Client(ctx)
-	resp, err := client.Post(compileURL, "text/plain", r.Body)
+
+	body := io.Reader(r.Body)
+	var input []byte
+	if r.ContentLength >= 0 && r.ContentLength < 250 {
+		var err error
+		input, err = ioutil.ReadAll(body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// check cached result
+		if item, err := memcache.Get(ctx, string(input)); err == nil {
+			var r result
+			if err := json.Unmarshal(item.Value, &r); err == nil {
+				w.WriteHeader(r.Status)
+				w.Write(r.Message)
+				return
+			}
+		}
+
+		body = bytes.NewBuffer(input)
+	}
+
+	resp, err := client.Post(compileURL, "text/plain", body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer resp.Body.Close()
+
+	if resp.ContentLength >= 0 && resp.ContentLength < 500000 {
+		output, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if bs, err := json.Marshal(&result{
+			Status:  resp.StatusCode,
+			Message: output,
+		}); err == nil {
+			memcache.Set(ctx, &memcache.Item{
+				Key:   string(input),
+				Value: bs,
+			})
+		}
+		w.WriteHeader(resp.StatusCode)
+		w.Write(output)
+		return
+	}
 
 	w.WriteHeader(resp.StatusCode)
 	io.Copy(w, resp.Body)
